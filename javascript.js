943a8d0b8bf38b847818951bea699e0f30da3da2
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-01 09:09:36 +0530

    added time on index.html, modified javascript file

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    now = new Date();
+    document.querySelector('#now').innerHTML = now;
+    })
+    
+
+console.log((4 + 6 + 9) / 77)
+
+document.write("hello world! Now ")
+//document.write(new Date())
+
